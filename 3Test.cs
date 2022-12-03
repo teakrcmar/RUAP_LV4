a9a0b7c60416c2db3da03815e47f797d5601e085
Tea <74956679+teakrcmar@users.noreply.github.com>
2022-12-03 10:55:42 +0100

    Add files via upload

---
+++ 3Test.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class 3Test
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+        
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+        
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        
+        [Test]
+        public void The3Test()
+        {
+            driver.FindElement(By.Id("FirstName")).Click();
+            driver.FindElement(By.Id("FirstName")).Clear();
+            driver.FindElement(By.Id("FirstName")).SendKeys("Test");
+            driver.FindElement(By.Id("LastName")).Clear();
+            driver.FindElement(By.Id("LastName")).SendKeys("Example");
+            driver.FindElement(By.Id("Email")).Clear();
+            driver.FindElement(By.Id("Email")).SendKeys("mailforme34@gmail.com");
+            driver.FindElement(By.Id("Password")).Click();
+            driver.FindElement(By.Id("Password")).Clear();
+            driver.FindElement(By.Id("Password")).SendKeys("testPassword123");
+            driver.FindElement(By.Id("ConfirmPassword")).Clear();
+            driver.FindElement(By.Id("ConfirmPassword")).SendKeys("testPassword123");
+            driver.FindElement(By.Id("register-button")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+        
+        private string CloseAlertAndGetItsText() {
+            try {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert) {
+                    alert.Accept();
+                } else {
+                    alert.Dismiss();
+                }
+                return alertText;
+            } finally {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
